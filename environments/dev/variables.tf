9a6c9b0e3885ee0e16390c3c07e2286f9d1265ac
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-28 12:51:42 -0600

    Create variables.tf

---
+++ environments/dev/variables.tf
@@ -0,0 +1,7 @@
+variable "bucket_name" {}
+variable "environment" {}
+variable "index_file" {}
+variable "zone_id" {}
+variable "record_name" {}
+variable "cloudfront_domain_name" {}
+variable "cloudfront_hosted_zone_id" {}
