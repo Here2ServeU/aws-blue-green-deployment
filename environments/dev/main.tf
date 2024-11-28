bf845e61217c17e6005d6bb118da825eac3ea317
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-28 13:00:34 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,12 +1,12 @@
 module "s3_bucket" {
-  source        = "../modules/s3-buckets"
+  source        = "../../modules/s3-buckets"
   bucket_name   = var.bucket_name
   environment   = var.environment
   index_file    = var.index_file
 }
 
 module "route53" {
-  source                = "../modules/route53"
+  source                = "../../modules/route53"
   zone_id               = var.zone_id
   record_name           = var.record_name
   cloudfront_domain_name = var.cloudfront_domain_name
