3a08c16587055b19c8e072faad02a280caf04bfd
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-28 14:14:00 -0600

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -12,3 +12,23 @@ variable "index_file" {
   description = "Path to the index file"
   type        = string
 }
+
+variable "zone_id" {
+  description = "Route 53 Hosted Zone ID"
+  type        = string
+}
+
+variable "record_name" {
+  description = "DNS record name"
+  type        = string
+}
+
+variable "cloudfront_domain_name" {
+  description = "Domain name of the CloudFront distribution"
+  type        = string
+}
+
+variable "cloudfront_hosted_zone_id" {
+  description = "Hosted Zone ID for CloudFront"
+  type        = string
+}
