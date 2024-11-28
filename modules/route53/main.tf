7cfeac7c8d64abc4309643bffb3c2ef4136de187
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-28 12:54:55 -0600

    Create main.tf

---
+++ modules/route53/main.tf
@@ -0,0 +1,10 @@
+resource "aws_route53_record" "blue_green_record" {
+  zone_id = var.zone_id
+  name    = var.record_name
+  type    = "A"
+  alias {
+    name                   = var.cloudfront_domain_name
+    zone_id                = var.cloudfront_hosted_zone_id
+    evaluate_target_health = true
+  }
+}
