51de4b0a78349addae86c52209d3409eaaf3089b
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-28 23:26:46 -0600

    Update main.tf

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,11 +1,17 @@
+# Create a Hosted Zone
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+}
+
+# Create a DNS A Record
 resource "aws_route53_record" "active_record" {
-  zone_id = var.zone_id
+  zone_id = aws_route53_zone.main.id
   name    = var.record_name
   type    = "A"
 
   alias {
     name                   = var.active_environment == "blue" ? var.blue_bucket_url : var.green_bucket_url
-    zone_id                = var.zone_id
+    zone_id                = var.cloudfront_hosted_zone_id  # Replace with CloudFront hosted zone ID
     evaluate_target_health = false
   }
 }
