80e5457aa2cdadc14e9b87be704f1bb72c008eb8
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-28 23:07:09 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,8 +1,10 @@
-module "s3_bucket" {
-  source        = "../../modules/s3-buckets"
-  bucket_name   = var.bucket_name
-  environment   = var.environment
-  index_file    = "${path.module}/files/index-blue.html"
+module "s3_buckets" {
+  source             = "../../modules/s3-buckets"
+  blue_bucket_name   = "t2s-dev-blue-bucket"
+  green_bucket_name  = "t2s-dev-green-bucket"
+  blue_index_file    = "${path.module}/../files/index-blue.html"
+  green_index_file   = "${path.module}/../files/index-green.html"
+  environment        = "dev"
 }
 
 module "route53" {
