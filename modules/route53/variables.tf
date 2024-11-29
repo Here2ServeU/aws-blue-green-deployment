51a1cb3be503766e8f41a8543ce81b7a2463425f
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-28 23:18:55 -0600

    Update variables.tf

--- modules/route53/variables.tf
+++ modules/route53/variables.tf
@@ -1,19 +1,24 @@
+variable "blue_bucket_url" {
+  description = "Website endpoint for the Blue bucket"
+  type        = string
+}
+
+variable "green_bucket_url" {
+  description = "Website endpoint for the Green bucket"
+  type        = string
+}
+
+variable "active_environment" {
+  description = "Specify active environment (blue or green)"
+  type        = string
+}
+
 variable "zone_id" {
-  description = "Route 53 Hosted Zone ID"
+  description = "Route 53 hosted zone ID"
   type        = string
 }
 
 variable "record_name" {
   description = "DNS record name"
-  type        = string
-}
-
-variable "cloudfront_domain_name" {
-  description = "Domain name of the CloudFront distribution"
-  type        = string
-}
-
-variable "cloudfront_hosted_zone_id" {
-  description = "Hosted Zone ID for CloudFront"
   type        = string
 }
