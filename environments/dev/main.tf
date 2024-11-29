4c38be6045394a851efc476b2c9915865ba9927a
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-29 00:03:00 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -9,8 +9,8 @@ module "s3_buckets" {
 
 module "route53" {
   source                  = "../../modules/route53"
-  domain_name             = "t2s-services.example.com" # Replace with your domain name
-  record_name             = "www.t2s-services.example.com" # Replace with your record name
+  domain_name             = "t2s-services.com" # Replace with your domain name
+  record_name             = "www.t2s-services.com" # Replace with your record name
   blue_bucket_url         = module.s3_buckets.blue_bucket_url
   green_bucket_url        = module.s3_buckets.green_bucket_url
   active_environment      = var.active_environment
