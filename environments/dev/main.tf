8aa7ad68a0437ce3bc732d37da46e4a71b45e9bf
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-30 10:29:34 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,15 +13,8 @@ module "route53" {
   record_name             = var.record_name # Replace with your record name
   blue_bucket_url         = module.s3_buckets.blue_bucket_url
   green_bucket_url        = module.s3_buckets.green_bucket_url
-  blue_cloudfront_url     = module.cloudfront.blue_domain_name
-  green_cloudfront_url    = module.cloudfront.green_domain_name
   active_environment      = var.active_environment
   cloudfront_hosted_zone_id = "Z2FDTNDATAQYW2" # Default hosted zone ID for CloudFront
 }
 
-module "cloudfront" {
-  source = "../../modules/cloudfront"
 
-  blue_distribution_id  = "E19TXOBXEAB3RX" # Replace with actual ID
-  green_distribution_id = "E18XO592ZPE2MQ" # Replace with actual ID
-}
