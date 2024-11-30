520ad52c11cbbbed2b887d46679f1be047bc2c75
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-30 10:04:39 -0600

    Create main.tf

---
+++ modules/cloudfront/main.tf
@@ -0,0 +1,7 @@
+resource "aws_cloudfront_distribution" "blue" {
+  # Placeholder for the imported CloudFront distribution for Blue
+}
+
+resource "aws_cloudfront_distribution" "green" {
+  # Placeholder for the imported CloudFront distribution for Green
+}
