0af5fce7d59b6d5172c4a3232e67c0b9e3dbddbc
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-30 10:04:57 -0600

    Create variables.tf

---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,9 @@
+variable "blue_distribution_id" {
+  description = "The CloudFront distribution ID for the Blue environment"
+  type        = string
+}
+
+variable "green_distribution_id" {
+  description = "The CloudFront distribution ID for the Green environment"
+  type        = string
+}
